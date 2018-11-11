b494ff87d2f2f20af6821e5eea38c2dd6a3a0236
Vishnu-Thirumalai <Vishnu-Thirumalai@users.noreply.github.com>
2018-11-11 01:29:58 +0100

    Full Basic Functionality including command line arguments

---
+++ cellularMazeGen.py
@@ -0,0 +1,77 @@
+import random
+import sys
+
+class mazeGeneration:
+
+	def __init__(self, size, inital, life, death):
+		self.size = size
+		self.initial = 0.4
+		self.done = False
+
+		self.deathLimit = 2
+		self.birthLimit = 4 
+
+		self.neighbours = [ (x,y) for x in range(-1,2) for y in range(-1,2)]
+		self.neighbours.remove((0,0))	
+	
+		self.map = [[ (False if random.random()>self.initial else True) for x in range(size)] for y in range(size)]
+
+	def main(self):
+		
+		while not self.done:
+			
+			self.iterate()
+		
+		for x in range(self.size):
+			self.map[0][x] = False
+			self.map[self.size-1][x] = False
+			self.map[x][0] = False
+			self.map[x][self.size-1] = False	
+
+		self.displayMap()	
+
+			
+	def displayMap(self):
+		for row in self.map:
+			for pos in row:
+				print ( " " if pos else "0" , end = " ")
+			print ("")
+		print("")
+
+	def add(self,a,b):
+		return (a[0]+b[0], a[1]+b[1])
+
+	def iterate(self):
+		self.done = True
+		for y in range(self.size):
+			for x in range(self.size):
+				val = 0
+				curr = self.map[y][x]
+				for bias in self.neighbours:
+					n = self.add((x,y),bias)
+					try:
+						if self.map[n[1]][n[0]]:
+							val = val + 1
+					except IndexError:
+						pass
+				if val < self.deathLimit:
+					self.map[y][x] = False
+				elif val > self.birthLimit:
+					self.map[y][x] = True	
+				
+				if self.map[y][x] != curr:
+					self.done = False
+
+
+
+args = [100, 0.4, 3, 4]
+try:
+	args[0] = int(sys.argv[1])
+	args[1] = float(sys.argv[2])
+	args[2] = int(sys.argv[3])
+	args[3] = int(sys.argv[4])
+except IndexError:
+	pass	
+
+m = mazeGeneration(*args)
+m.main()							
